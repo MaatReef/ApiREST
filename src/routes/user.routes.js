218653f395251cd3a45c899cfdee54a4c9e0ee40
maatreef <maatreef33@gmail.com>
2022-12-31 08:17:53 -0300

    [ADD] authentication implemented

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require ("express");
+// El de abajo lo usamo como método de protección de ruta, si son varios middlewares se ponen entre []
+const { AuthMiddleWare } = require("../middlewares");
 
 module.exports = function({ UserController }){
     const router = Router();
 
+    router.get("", AuthMiddleWare, UserController.getAll);
     router.get("/:userId", UserController.get);
-    router.get("", UserController.getAll);
     router.patch("/:userId", UserController.update);
     router.delete("/:userId", UserController.delete);
 
