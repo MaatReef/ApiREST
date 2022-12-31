590e98a69d5a3699fe22505867fa025d078ab0da
maatreef <maatreef33@gmail.com>
2022-12-31 10:52:45 -0300

    [ADD] tests added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        _id: "63b0353aa7d1f60f32f921a2",
+        name: "Maat",
+        username: "MaatUsername",
+        password: "MyStrongPassword",
+    },
+    users: [
+        {
+            _id: "63b03800e4284b42465d98a3",
+            name: "Folk1",
+            username: "FolkUser1",
+            password: "MyFolk1",
+        },
+        {
+            _id: "63b03800e4284b42465d98a4",
+            name: "Folk2",
+            username: "FolkUser2",
+            password: "MyFolk2",
+        }
+    ]
+}
